b0cb7c225a516e87c9751bd0bf44ae3e1f9129b5
Mikias Abera <itsmikias@gmail.com>
2016-05-31 12:25:05 +0300

    Add file thumbnails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,10 @@ module ApplicationHelper
   def disable_sidebar
     @disable_sidebar = true
   end
+
+  # Formating
+  def prettify(amount)
+    number_to_currency(amount, unit: "", separator: ".", delimiter: ",", significant: false)
+  end
+  
 end
