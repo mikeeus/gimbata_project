b13994927575c5d0515bdddc3bda3d87bcb72180
Mikias Abera <itsmikias@gmail.com>
2016-06-01 12:48:22 +0300

    Add styling and precompile assets

--- app/assets/javascripts/plugins.js
+++ app/assets/javascripts/plugins.js
@@ -1,20 +1,18 @@
 $(document).ready(function(){
   
   // Initialize bootstrap-material-design
-  $.material.init();
-
-  
+  // $.material.init();
 
 });
 
 // Custom Scrollbar
 $(window).load(function(){
 
-  $('.nav-side-menu').mCustomScrollbar({
-    axis: 'y',
-    scrollInertia: 400,
-    mouseWheel: true,
-    autoHideScrollbar: true
-  });
+  // $('.nav-side-menu').mCustomScrollbar({
+  //   axis: 'y',
+  //   scrollInertia: 400,
+  //   mouseWheel: true,
+  //   autoHideScrollbar: true
+  // });
   
 });
