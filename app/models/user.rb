4081d0587d420bb2d769373ce6f3c2d06658db4e
Mikias Abera <itsmikias@gmail.com>
2016-06-02 19:19:11 +0300

    Add drag and drop upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Make confirmable?
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :company
+  validates_attachment :file,
+      styles: { thumb: "100x100#", small: "150x150#"},
+      content_type: { content_type: [
+        "image/jpg", "image/jpeg", "image/png", "image/gif"]},
+      message: "Sorry, only Images png, gif, jpeg or png."
 
   enum role: [:standard, :reporting, :admin]
 end
