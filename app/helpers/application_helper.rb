f4c4a54ef66ccf37b4be3230be1aed6bcec35981
Mikias Abera <itsmikias@gmail.com>
2016-06-03 11:57:35 +0300

    Add homepage styling, refactoring and title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,4 +28,12 @@ module ApplicationHelper
     number_to_currency(amount, unit: "", separator: ".", delimiter: ",", significant: false)
   end
   
+  def full_title(page_title = '')
+    base_title = "mesabya"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
