eccd1eb0f8796a9f967aaee22356465d40ef3ca9
Mikias Abera <itsmikias@gmail.com>
2016-06-06 08:48:40 +0300

    Add s3 credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,11 +81,13 @@ Rails.application.configure do
   # Paperclip and AWS S3 configuration
   config.paperclip_defaults = {
     storage: :s3,
+    endpoint: 'gimbata.s3-website.eu-central-1.amazonaws.com',
+    s3_host_name: 's3-eu-central-1.amazonaws.com',
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-    }
+      s3_region: ENV.fetch('AWS_REGION')
+    },
+    bucket: ENV.fetch('S3_BUCKET_NAME')
   }
 end
